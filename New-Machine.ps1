a6f9775e478b162b379ea6190bc3c3855cebffb7
Fredrik Öller <fredrik.oller@teliacompany.com>
2025-01-14 10:01:17 +0100

    editorconfig and winget

--- New-Machine.ps1
+++ New-Machine.ps1
@@ -5,24 +5,32 @@ wsl --set-default-version 2
 # Install Ubuntu
 wsl --install -d Ubuntu
 
-# Install Chocolatey (Windows package manager)
-Set-ExecutionPolicy Bypass -Scope Process -Force; 
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# Install PowerShell using winget
+winget install Microsoft.PowerShell -e --accept-package-agreements -h
+winget install Microsoft.Git -e --accept-package-agreements -h
+winget install Microsoft.WindowsTerminal -e -h --accept-package-agreements
+winget install Microsoft.Teams -e -h --accept-package-agreements
+winget install Microsoft.PowerToys -e -h --accept-package-agreements
+winget install Microsoft.Microsoft.AzureCLI -e -h --accept-package-agreements
+winget install Microsoft.VisualStudioCode -e -h --accept-package-agreements
+winget install Microsoft.VisualStudio.2022.Community -e -h --accept-package-agreements
+winget install JanDeDobbeleer.OhMyPosh -e -h --accept-package-agreements
+winget install Microsoft.Azure.FunctionsCoreTools -e -h --accept-package-agreements
+winget install Microsoft.AzureDataStudio -e -h --accept-package-agreements
+winget install Microsoft.Azure.StorageEmulator -e -h --accept-package-agreements
+winget install Microsoft.Azure.StorageExplorer -e -h --accept-package-agreements
+winget install Bruno.Bruno -e -h --accept-package-agreements
 
-choco install microsoft-teams -y
-choco install powertoys -y
-choco install microsoft-windows-terminal -y
-choco install azurevpn -y
-choco install azure-cli -y
-choco install vscode -y
-choco install visualstudio2022community -y
-choco install spotify -y
-choco install oh-my-posh -y
-choco install slack -y
 
-# Doenload CaskadiyaCove
-Invoke-WebRequest -Uri "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CaskaydiaCove.zip" -OutFile "$env:TEMP\CaskaydiaCove.zip"
+# For Funzies
+winget install Blizzard.BattleNet -e -h --accept-package-agreements
+winget install EpicGames.EpicGamesLauncher -e -h --accept-package-agreements
+winget install Valve.Steam -e -h --accept-package-agreements
+winget install GOG.Galaxy -e -h --accept-package-agreements
+
+
+# Download CaskadiyaCove
+Invoke-WebRequest -Uri "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CascadiaMono.zip" -OutFile "$env:TEMP\CaskaydiaCove.zip"
 Expand-Archive -Path "$env:TEMP\CaskaydiaCove.zip" -DestinationPath "$env:TEMP\CaskaydiaCove"
 Copy-Item "$env:TEMP\CaskaydiaCove\*.ttf" -Destination "$env:WINDIR\Fonts"
 
